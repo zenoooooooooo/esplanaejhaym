eadaa91a490676fa41bf9ffeea37e891762d7404
E-Jhay Esplana <esplanaejhaym@gmail.com>
2025-01-22 06:51:15 +0800

    experimenting something new

--- src/app/components/Model.tsx
+++ src/app/components/Model.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
+import * as THREE from "three";
+import { useFrame, useThree } from "@react-three/fiber";
 
 interface ModelProps {
   scale?: number;
@@ -10,6 +13,29 @@ interface ModelProps {
 
 const Model: React.FC<ModelProps> = ({ scale, position, rotation, glb }) => {
   const { scene } = useGLTF(glb);
+  const raycaster = new THREE.Raycaster();
+  const { gl, camera } = useThree();
+
+  useEffect(() => {
+    const onMouseMove = (event: MouseEvent) => {
+      const coords = new THREE.Vector2(
+        (event.clientX / gl.domElement.clientWidth) * 2 - 1,
+        -(event.clientY / gl.domElement.clientHeight) * 2 - 1
+      );
+      raycaster.setFromCamera(coords, camera);
+
+      const intersections = raycaster.intersectObjects(scene.children, true);
+      if (intersections.length > 0) {
+        console.log(intersections[0]);
+      }
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", onMouseMove);
+    };
+  }, [gl]);
+
   return (
     <primitive
       object={scene}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,17 @@
 "use client";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import Model from "./components/Model";
 import CanvasLoader from "./components/CanvasLoader";
 import { Vector3 } from "three";
 import * as THREE from "three";
+
 export default function Home() {
   const [lightPosition, setLightPosition] = useState<Vector3 | undefined>(
     undefined
   );
-
-  useEffect(() => {
-    const handleMouseMove = (event: MouseEvent) => {
-      let x = event.clientX / window.innerWidth;
-      let y = -(event.clientY / window.innerHeight);
-      const z = 10;
-      x = x <= 0 ? x : -x;
-      y = y <= 0 ? y : -y;
-      setLightPosition(new THREE.Vector3(x, -y, z));
-    };
-
-    window.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
 
   return (
     <>
@@ -41,12 +25,11 @@ export default function Home() {
         }}
       >
         <spotLight
+          position={lightPosition}
           intensity={100}
-
           angle={Math.PI / 25}
           penumbra={0.1}
           decay={2}
-          lookAt={() => lightPosition}
           castShadow
         />
         <directionalLight
@@ -63,8 +46,8 @@ export default function Home() {
             glb="/models/MyRoom.glb"
           />
         </Suspense>
-        {/* <PerspectiveCamera makeDefault fov={50} position={[7.3, 5, 7.3]} /> */}
-        <PerspectiveCamera makeDefault fov={50} position={[0, 0, 5]} />
+        <PerspectiveCamera makeDefault fov={50} position={[7.3, 5, 7.3]} />
+        {/* <PerspectiveCamera makeDefault fov={50} position={[0, 0, 5]} /> */}
         <OrbitControls
           enableZoom={false}
           enableRotate={false}
