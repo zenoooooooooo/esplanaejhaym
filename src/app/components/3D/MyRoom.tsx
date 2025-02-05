295eb614dc3e232790b7f0456999e00471905182
E-Jhay Esplana <esplanaejhaym@gmail.com>
2025-02-06 00:01:58 +0800

    add touch event for flashlight in room for mobiles

--- src/app/components/3D/MyRoom.tsx
+++ src/app/components/3D/MyRoom.tsx
@@ -3,18 +3,14 @@ import React, { useEffect, useState } from "react";
 import { useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 import { useFrame, useThree, Vector3 } from "@react-three/fiber";
-
+import { useMediaQuery } from "react-responsive";
 interface MyRoomProps {
   scale?: number;
   position?: number[];
   rotation?: number[];
 }
 
-const MyRoom: React.FC<MyRoomProps> = ({
-  scale,
-  position,
-  rotation,
-}) => {
+const MyRoom: React.FC<MyRoomProps> = ({ scale, position, rotation }) => {
   const { scene } = useGLTF("./models/MyRoom.glb");
   const raycaster = new THREE.Raycaster();
   const { gl, camera } = useThree();
@@ -22,12 +18,29 @@ const MyRoom: React.FC<MyRoomProps> = ({
     THREE.Vector3 | undefined
   >(undefined);
 
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+  const isDesktop = useMediaQuery({ minWidth: 1025 });
+
   const [light, setLight] = useState(false);
   useEffect(() => {
-    const onMouseMove = (event: MouseEvent) => {
+    const onPointerMove = (e: MouseEvent | TouchEvent) => {
+      let clientX, clientY;
+
+      if (e instanceof MouseEvent) {
+        clientX = e.clientX;
+        clientY = e.clientY;
+      } else if (e instanceof TouchEvent) {
+        clientX = e.touches[0].clientX;
+        clientY = e.touches[0].clientY;
+      } else {
+        return;
+      }
+
       const mouse = new THREE.Vector2(
-        (event.clientX / gl.domElement.clientWidth) * 2 - 1,
-        -(event.clientY / gl.domElement.clientHeight) * 2 + 1
+        (clientX / gl.domElement.clientWidth) * 2 - 1,
+        -(clientY / gl.domElement.clientHeight) * 2 + 1
       );
 
       raycaster.setFromCamera(mouse, camera);
@@ -46,15 +59,20 @@ const MyRoom: React.FC<MyRoomProps> = ({
       }
     };
 
-    window.addEventListener("mousemove", onMouseMove);
-    window.addEventListener("click", toggleLight);
-    return () => {
-      window.removeEventListener("mousemove", onMouseMove);
-      window.removeEventListener("click", toggleLight);
-    };
+    if (isSmall && isMobile && isTablet) {
+      window.addEventListener("touchmove", onPointerMove);
+      window.addEventListener("touchstart", toggleLight);
+    } else if (isDesktop) {
+      window.addEventListener("mousemove", onPointerMove);
+      window.addEventListener("click", toggleLight);
+      return () => {
+        window.removeEventListener("mousemove", onPointerMove);
+        window.removeEventListener("click", toggleLight);
+      };
+    }
   }, [gl, camera, scene]);
 
-  function toggleLight(e: MouseEvent) {
+  function toggleLight(e: MouseEvent | TouchEvent) {
     e.preventDefault();
     setLight((prev) => !prev);
   }
