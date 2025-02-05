4db346f41f8d9571efd7de62f6b4776bd209e6c7
E-Jhay Esplana <esplanaejhaym@gmail.com>
2025-02-06 01:37:46 +0800

    used react icons for controller

--- src/app/aboutMe/page.tsx
+++ src/app/aboutMe/page.tsx
@@ -5,6 +5,7 @@ import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas, Vector3 } from "@react-three/fiber";
 import { CanvasLoader, MyRoom } from "../components";
 import { useMediaQuery } from "react-responsive";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 export default function AboutMe() {
   const [camPos, setCamPos] = useState(0);
@@ -26,19 +27,34 @@ export default function AboutMe() {
         <Suspense fallback={<CanvasLoader />}>
           <MyRoom scale={4} position={[1, -1, -13]} rotation={[0.5, -0.8, 0]} />
         </Suspense>
-        <PerspectiveCamera makeDefault fov={isDesktop ? 100 : isTablet ? 100 : 120} position={[camPos, 0, 1]} />
+        <PerspectiveCamera
+          makeDefault
+          fov={isDesktop ? 100 : isTablet ? 90 : 80}
+          position={[camPos, 0, 1]}
+        />
         <OrbitControls
           enableZoom={false}
           enableRotate={false}
           enablePan={false}
         />
       </Canvas>
-
-      <div className="absolute top-20 right-10 text-2xl text-white x-[9999]">
-        <button onClick={() => setCamPos(camPos - 0.01)}>&gt;</button>
-      </div>
-      <div className="absolute top-20 left-10 text-2xl text-white x-[9999]">
-        <button onClick={() => setCamPos(camPos + 0.01)}>&lt;</button>
+      <div className="desktop:hidden text-4xl text-gray-700 x-[9999]">
+        <span className="absolute top-20 right-10 ">
+          <button
+            onClick={() => setCamPos(camPos - 0.01)}
+            disabled={camPos <= -0.2}
+          >
+            <FaArrowAltCircleRight />
+          </button>
+        </span>
+        <span className="absolute top-20 left-10">
+          <button
+            onClick={() => setCamPos(camPos + 0.01)}
+            disabled={camPos >= 0.2}
+          >
+            <FaArrowAltCircleLeft />
+          </button>
+        </span>
       </div>
     </>
   );
