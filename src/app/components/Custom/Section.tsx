21fe4cacf2cda9995231e86224d0b948d7e73306
E-Jhay Esplana <esplanaejhaym@gmail.com>
2025-02-05 23:44:04 +0800

    edited custom section component

--- src/app/components/Custom/Section.tsx
+++ src/app/components/Custom/Section.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { ElementType, ReactNode } from "react";
 
 interface SectionProps {
-  text: string;
-  children: React.ReactNode;
-  className: string;
+  text?: string;
+  children?: ReactNode;
+  className?: string;
+  tag?: ElementType;
 }
 
-const Section: React.FC<SectionProps> = ({ text, children, className }) => {
-  return <section>Section</section>;
+const Section: React.FC<SectionProps> = ({
+  text,
+  children,
+  className = "",
+  tag: Tag = "div",
+}) => {
+  return (
+    <Tag className={className}>
+      {text && <p>{text}</p>}
+      {children}
+    </Tag>
+  );
 };
 
 export default Section;
